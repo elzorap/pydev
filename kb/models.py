39b627febb2cd6455341d2fadc172eb287b399d8
Mihai Ionescu <ionescumyhay@yahoo.com>
2023-01-12 15:32:41 +0200

    Create Article model and migrate to database

--- kb/models.py
+++ kb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
+from django.utils import timezone
 # Create your models here.
+
+
+class Article(models.Model):
+    author = ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    
